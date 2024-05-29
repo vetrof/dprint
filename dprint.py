82a8f17d398a1f035443ae014e3d675bdda82000
Vitaly L <111348000+vetrof@users.noreply.github.com>
2024-05-29 23:01:44 +0500

    Initial commit

---
+++ dprint.py
@@ -0,0 +1,29 @@
+import inspect
+import os
+
+
+def dprint(value):
+    # get call
+    frame = inspect.currentframe().f_back
+    # get file and line
+    absolute_filename = frame.f_code.co_filename
+    lineno = frame.f_lineno
+    # get root
+    project_root = os.getcwd()
+    # get path
+    relative_filename = os.path.relpath(absolute_filename, project_root)
+    # get all variable in file
+    local_vars = frame.f_locals
+
+    # find var in indetnt id obj
+    var_name = None
+    for name, val in local_vars.items():
+        if id(val) == id(value):
+            var_name = name
+            break
+
+    if var_name:
+        print(f">>>>>>>>>>>>> {var_name} = {value} ({relative_filename} : {lineno})")
+    else:
+        print(f">>>>>>>>>>>>> Value = {value} ({relative_filename} : {lineno})")
+
---
+++ readme.md
@@ -0,0 +1,16 @@
+### dprint
+
+Simple helper for print debug!
+print variable name, value, path and line in file.
+
+`x = "yohoho"`  
+`print(x)`
+
+output: `yohoho`
+
+or 
+
+`x = "yohoho"`  
+`dprint(x)`
+
+output: `>>>>>>>>>>>>> xyz = yohoho (test.py : 5)`
